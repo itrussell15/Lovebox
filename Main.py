4b134c550021c2d6f49076be3f6fba27c3c64540
Isaac <itrussell15@gmail.com>
2021-07-24 23:26:25 -0600

    Migrating to periodic multithreaded checks of messages and constant upating to UI

--- Main.py
+++ Main.py
@@ -6,12 +6,14 @@ Created on Sun Jul 11 16:26:46 2021
 """
 
 from UI_Handler import UI_Handler
-from GPIO_Handler import GPIO_Handler
+from MessageRequest import MessageRequest
+# from GPIO_Handler import GPIO_Handler
 import time
+from threading import Timer
 import logging
 
-url = "https://itrussell15.pythonanywhere.com/"
-# url = "http://127.0.0.1:5000/"
+# url = "https://itrussell15.pythonanywhere.com/"
+url = "http://127.0.0.1:5000/"
 
 def setup_logging():
     log_format = '%(asctime)s %(message)s'
@@ -21,19 +23,44 @@ def setup_logging():
                         level = logging.INFO)    
     return logging.getLogger("LoveboxLogger")
 
+
 ui = UI_Handler("Land_Of_Schmucks", url)
-gpio = GPIO_Handler(led = 18, light = 4)
-log = setup_logging()
-message = False
+ui.timer.start()
+# t = Timer(5, ui.check_new_message)
+# t.start()
+
+# req = MessageRequest(url, "Land_Of_Schmucks", "password")
+# out = req.get_messages()
+
+
+
+
+# gpio = GPIO_Handler(led = 18, light = 4)
+# log = setup_logging()
+# message = False
 
 while True:
-    state = ui.check_new_message()
-    if message:
-        gpio.flash_light()
-        print("Message Queued, waiting for input")
-        gpio.light.wait_for_dark()
-        print("Message read")
-        ui.read_message()
-        time.sleep(0.5 )
-    time.sleep(1)
+    pass
+    
+#     if ui.check_new_message() or ui.unread_messages:
+#         print(ui.unread_messages)
+#         message_id = ui.queue_message()
+#         print("Message Queued, Waiting for input")
+#         a = input()
+#         ui.read_message(message_id)
+#     # else:
+#     #     ui.output_no_message()
+#     time.sleep(60)
+
+# while True:
+#     state = ui.check_new_message()
+#     if message:
+#         ui.queue_message()
+#         gpio.flash_light()
+#         print("Message Queued, waiting for input")
+#         gpio.light.wait_for_dark()
+#         print("Message read")
+#         ui.read_message()
+#         time.sleep(0.5 )
+#     time.sleep(1)
 
--- UI_Handler.py
+++ UI_Handler.py
@@ -9,38 +9,58 @@ from MessageRequest import MessageRequest
 import os, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from threading import Thread
 
 class UI_Handler:
     
     def __init__(self, username, url):
         self.url = url
-        self.req = MessageRequest(url, username, "asklfghalskgha")
+        self.req = MessageRequest(url, username, "password")
         self.driver = self._create_driver()
+        self.SetTimer()
         self.show_output()
-        self.current_message = None
+        self.unread_messages = {}
+        
+    class Timer(Thread):
+        def __init__(self, event, function, interval = 60):
+            Thread.__init__(self)
+            self.stopped = event
+            self.function = function
+            self.interval = interval
+        
+        def run(self):
+            while not self.stopped.wait():
+                self.function()
         
     def _create_driver(self):
         print("Creating driver")
         options = Options()
-        options.add_argument("--kiosk")
+        # options.add_argument("--kiosk")
         return webdriver.Chrome(executable_path=os.getcwd() + "\\chromedriver.exe", options = options)
         # return webdriver.Chrome(executable_path="usr/lib/chromium-browser/chromedriver", options = options)
         
     def check_new_message(self):
         # print("Checking Messages")
         r = self.req.get_messages()
+        print("Message Request made")
         if "unread" in r.keys():
-            message = r["unread"][0]
-            self.create_message_html(message)
-            self.current_message = message
+            messages = r["unread"]
+            self.unread_messages = messages
             return True
         else:
             self.output_no_message()
-            self.current_message = None
+            self.messages = {}
             return False
+
+    def queue_message(self):
+        keys = list(self.unread_messages.keys())
+        out = self.unread_messages[keys[0]]
+        self.create_message_html(out)
+        return keys[0]
         
-    def read_message(self):
-        self.req.message_read(self.current_message["id"])
+    def read_message(self, m_id):
+        self.req.message_read(m_id)
+        self.unread_messages.pop(m_id)
         self.current_message = None
         
     def create_message_html(self, message):
@@ -57,6 +77,9 @@ class UI_Handler:
         with open("templates\\output.html", "w") as f:
             f.write(file)
         self.driver.refresh()
-            
+         
     def show_output(self):
-        self.driver.get("file://{}".format(os.getcwd() + "\\templates\\output.html"))       
+        self.driver.get("file://{}".format(os.getcwd() + "\\templates\\output.html"))  
+        
+    def SetTimer(self):
+        
